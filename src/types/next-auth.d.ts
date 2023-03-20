7bb3401b176a4e14d10c6e1826e1bff8bb785bae
James Eriksson <ja.mera@hotmail.com>
2023-03-20 19:06:14 +0100

    File contained wrong code, fixed

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,14 +1,9 @@
-import { compare, hash } from 'bcryptjs';
+import { type DefaultSession } from "next-auth";
 
-export async function hashPassword(password: string): Promise<string> {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-  const hashedPassword: string = await hash(password, 12);
-  return hashedPassword;
-}
-
-export async function verifyPassword(password: string | null, hashedPassword: string | null): Promise<boolean> {
-  if(!hashedPassword || !password) return false;
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-  const isValid: boolean = await compare(password, hashedPassword);
-  return isValid;
+declare module "next-auth" {
+  interface Session {
+    user?: {
+      id: string;
+    } & DefaultSession["user"];
+  }
 }
