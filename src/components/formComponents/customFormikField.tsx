6aa9bca205bfc889098272c0a243813ad68678be
James Eriksson <ja.mera@hotmail.com>
2023-03-20 23:14:31 +0100

    Styling changes

--- src/components/formComponents/customFormikField.tsx
+++ src/components/formComponents/customFormikField.tsx
@@ -17,8 +17,8 @@ export const CustomFormikField: FC<CustomFormikFieldProps & FieldProps> = ({
   return (
     <div className="relative flex flex-col">
       <label className="absolute capitalize text-gray-100 font-medium" htmlFor={field.name}>{field.name}</label>
-      <input className={`bg-purple-7 border-purple-6 border-2 rounded-md py-2 pl-3 mt-7 ${isError ? "bg-red-600 border-red-800" : ""}`} type={type} placeholder={placeholder} {...field} />
-      {true && <div className="text-xs font-bold text-red-600 mt-1">{errors[field.name] as string}Error</div>}
+      <input className={`border-2 rounded-md h-12 pl-3 mt-7 outline-none ${isError ? "bg-red-500 border-red-700 placeholder:text-gray-300 font-semibold focus:border-white" : "focus:border-purple-4 bg-purple-7 border-purple-6"} ${type === "password" ? "text-base pb-1" : "text-sm"}`} type={type} placeholder={placeholder} {...field} />
+      {isError && <div className="text-xs font-bold text-red-600 mt-1">{errors[field.name] as string}</div>}
     </div>
   )
 }
