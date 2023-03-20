025674eaafee5a09c3870891b310d57730fe5384
James Eriksson <ja.mera@hotmail.com>
2023-03-20 22:33:44 +0100

    Added styling to login page

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -29,15 +29,15 @@ export default function SignIn({ providers }: InferGetServerSidePropsType<typeof
         <title>Login</title>
       </Head>
       <div>
-        <h1>Welcome back!</h1>
-        <p>Sign in to your account</p>
+        <h1 className="text-4xl" >Welcome back!</h1>
+        <p className="text-xs text-gray-400 font-semibold">Sign in to your account</p>
         <Formik
           initialValues={{ email: '', password: '' }}
           onSubmit={submit}
         >
           {() => (
-            <Form>
-              <Field  
+            <Form className="flex flex-col">
+              <Field
                 placeholder="zyzz@mirin.com"
                 name="email"
                 type="email"
@@ -49,42 +49,45 @@ export default function SignIn({ providers }: InferGetServerSidePropsType<typeof
                 type="password"
                 component={CustomFormikField}
               />
-              <button type="submit">Sign In</button>
+              <button className="mt-3 bg-purple-3 py-2" type="submit">Sign In</button>
             </Form>
           )}
         </Formik>
       </div>
-      <div>
-        <div /> {/* Line */}
-        <p>or</p>
-        <div /> {/* Line */}
+      <div className="my-7 flex items-center">
+        <div className="h-0.5 bg-purple-2 flex-1" /> {/* Line */}
+        <p className="mx-2" >or</p>
+        <div className="h-0.5 bg-purple-2 flex-1" /> {/* Line */}
       </div>
-      <div>
+      <div className="flex flex-col" >
         {"github" in providers && (
           <button
+            className="mb-4 flex items-center justify-center bg-gray-600 py-2 font-semibold"
             onClick={() => pressedProvider("github")}
           >
-            Sign in with Github <RiGithubLine />
+            Sign in with Github <RiGithubLine className="ml-2" />
           </button>
         )}
         {"google" in providers && (
           <button
+            className="mb-4 flex items-center justify-center bg-orange-400 py-2 font-semibold"
             onClick={() => pressedProvider("google")}
           >
-            Sign in with Google <RiGithubLine />
+            Sign in with Google <BsGoogle className="ml-2" />
           </button>
         )}
         {"facebook" in providers && (
           <button
+            className="mb-4 flex items-center justify-center bg-blue-600 py-2 font-semibold"
             onClick={() => pressedProvider("facebook")}
           >
-            Sign in with Facebook <RiGithubLine />
+            Sign in with Facebook <BsFacebook className="ml-2" />
           </button>
         )}
       </div>
-      <div>
-        <p>
-          Don&apos;t have an account? <Link href="/auth/register">Sign Up Now</Link>
+      <div className="mt-3 flex justify-center">
+        <p className="text-gray-500">
+          Don&apos;t have an account? <Link className="text-white underline" href="/auth/register">Sign Up Now</Link>
         </p>
       </div>
     </SignLayout>
