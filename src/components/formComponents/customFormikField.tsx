60fa32d872f9b80bc56eb64bab2f23fe718d1bbb
James Eriksson <ja.mera@hotmail.com>
2023-03-20 22:57:17 +0100

    Added styling to the customFormikField

--- src/components/formComponents/customFormikField.tsx
+++ src/components/formComponents/customFormikField.tsx
@@ -15,10 +15,10 @@ export const CustomFormikField: FC<CustomFormikFieldProps & FieldProps> = ({
   const isError = touched[field.name] && errors[field.name];
 
   return (
-    <div>
-      <label htmlFor={field.name}>{field.name}</label>
-      <input type={type} placeholder={placeholder} {...field} />
-      {isError && <div>{errors[field.name] as string}</div>}
+    <div className="relative flex flex-col">
+      <label className="absolute capitalize text-gray-100 font-medium" htmlFor={field.name}>{field.name}</label>
+      <input className={`bg-purple-7 border-purple-6 border-2 rounded-md py-2 pl-3 mt-7 ${isError ? "bg-red-600 border-red-800" : ""}`} type={type} placeholder={placeholder} {...field} />
+      {true && <div className="text-xs font-bold text-red-600 mt-1">{errors[field.name] as string}Error</div>}
     </div>
   )
 }
