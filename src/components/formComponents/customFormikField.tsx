829547dd1a6f10ddffe021c843c53998c565cdfb
James Eriksson <ja.mera@hotmail.com>
2023-03-28 01:26:56 +0200

    Minor styling changes

--- src/components/formComponents/customFormikField.tsx
+++ src/components/formComponents/customFormikField.tsx
@@ -3,6 +3,7 @@ import { type FieldProps } from 'formik';
 
 interface CustomFormikFieldProps {
   type?: string;
+  label?: string;
   placeholder?: string;
   margin?: string;
 }
@@ -13,14 +14,15 @@ export const CustomFormikField: FC<CustomFormikFieldProps & FieldProps> = ({
   type = 'text',
   placeholder = '',
   margin = '',
+  label = '',
 }) => {
   const isError = touched[field.name] && errors[field.name];
 
   return (
     <div className={"relative flex flex-col " + margin}>
-      <label className="absolute capitalize text-gray-100 font-medium" htmlFor={field.name}>{field.name}</label>
+      <label className="absolute ml-1 capitalize text-gray-100 font-medium text-sm" htmlFor={field.name}>{label}</label>
       <input 
-        className={`border-2 rounded-md h-12 pl-3 mt-7 outline-none ${isError ? "bg-red-500 border-red-700 placeholder:text-gray-300 font-semibold focus:border-white" : "focus:border-purple-4 bg-purple-7 border-purple-6"} ${type === "password" ? "text-base pb-1" : "text-sm"}`} 
+        className={`border-2 rounded-md h-12 pl-3 mt-6 outline-none ${isError ? "bg-red-500 border-red-700 placeholder:text-gray-300 font-semibold focus:border-white" : "focus:border-purple-4 bg-purple-7 border-purple-6"} ${type === "password" ? "text-base pb-1" : "text-sm"}`} 
         type={type} placeholder={placeholder} {...field} />
       {isError && <div className="text-xs font-bold text-red-600 mt-1">{errors[field.name] as string}</div>}
     </div>
