b673170107ec00776e435cb77a6f91688e460526
James Eriksson <ja.mera@hotmail.com>
2023-04-09 00:31:39 +0200

    Minor UI changes

--- src/components/formComponents/customFormikField.tsx
+++ src/components/formComponents/customFormikField.tsx
@@ -22,7 +22,12 @@ export const CustomFormikField: FC<CustomFormikFieldProps & FieldProps> = ({
     <div className={"relative flex flex-col " + margin}>
       <label className="absolute ml-1 capitalize text-gray-100 font-medium text-sm" htmlFor={field.name}>{label}</label>
       <input 
-        className={`border-2 rounded-md h-12 pl-3 mt-6 outline-none ${isError ? "bg-red-500 border-red-700 placeholder:text-gray-300 font-semibold focus:border-white" : "focus:border-purple-4 bg-purple-7 border-purple-6"} ${type === "password" ? "text-base pb-1" : "text-sm"}`} 
+        className={`border-2 rounded-md h-12 pl-3 mt-6 outline-none focus:ring-0 focus:ring-offset-0 ${
+          isError ? 
+          "bg-red-500 border-red-700 placeholder:text-gray-300 font-semibold focus:border-white" 
+          : 
+          "focus:border-purple-4 bg-purple-7 border-purple-6"} 
+          ${type === "password" ? "text-base" : "text-sm"}`} 
         type={type} placeholder={placeholder} {...field} />
       {isError && <div className="text-xs font-bold text-red-600 mt-1">{errors[field.name] as string}</div>}
     </div>
